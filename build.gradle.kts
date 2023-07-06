385ab23500c8a88b9524056fe36a7536e4e81ad9
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-07-07 00:41:59 +0200

    Readd dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ spotless {
 
 
 dependencies {
-    //implementation("org.eclipse.jgit:org.eclipse.jgit:6.6.0.202305301015-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.6.0.202305301015-r")
     implementation("de.chojo", "cjda-util", "2.9.0+beta.11-DEV") {
         exclude(group = "club.minnced", module = "opus-java")
     }
