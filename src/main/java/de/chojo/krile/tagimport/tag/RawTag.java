c60da716406f696b7986100d5727678ca32681f4
Lilly <46890129+RainbowDashLabs@users.noreply.github.com>
2023-08-06 15:24:08 +0200

    Add 'enhanceMarkdown' feature to Krile
    This update introduces the 'enhanceMarkdown' functionality. It enhances GitHub Markdown into a more user-friendly and neat Discord Markdown format. The feature ensures that code blocks are kept together and line break behavior is consistent with GitHub Markdown.
    
    Detailed documentation of the feature has been added to 'meta.md', making it easy for users to understand.
    
    Changes have also been made to the 'RawTag.java' to include text splitting function, depending on the 'enhanceMarkdown' flag. This ensures different text treatments are tested thoroughly and the new feature works efficiently.
    
    This is a significant improvement in the user interface of the 'Krile' application.

--- src/main/java/de/chojo/krile/tagimport/tag/RawTag.java
+++ src/main/java/de/chojo/krile/tagimport/tag/RawTag.java
@@ -8,13 +8,16 @@ package de.chojo.krile.tagimport.tag;
 
 import de.chojo.krile.tagimport.tag.entities.FileMeta;
 import de.chojo.krile.tagimport.tag.entities.RawTagMeta;
-import de.chojo.krile.util.Text;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import static de.chojo.krile.util.Text.splitByLength;
+import static de.chojo.krile.util.Text.toDiscordMarkdownAndSplit;
+
 public record RawTag(RawTagMeta meta, FileMeta fileMeta, String text) {
+    private static final int MAX_LENGTH = 1850;
     /**
      * Splits the given text by the "<new_page>" delimiter and breaks each substring into smaller chunks,
      * each having a maximum length of 1850 characters.
@@ -22,6 +25,9 @@ public record RawTag(RawTagMeta meta, FileMeta fileMeta, String text) {
      * @return A list of strings, where each string represents a chunk of the original text.
      */
     public List<String> splitText() {
-        return Arrays.stream(text.split("<new_page>")).map(t -> Text.toDiscordMarkdownAndSplit(t, 1850)).flatMap(Collection::stream).toList();
+        return Arrays.stream(text.split("<new_page>"))
+                .map(t -> meta.enhanceMarkdown() ? toDiscordMarkdownAndSplit(t, MAX_LENGTH) : splitByLength(t, MAX_LENGTH))
+                .flatMap(Collection::stream)
+                .toList();
     }
 }
